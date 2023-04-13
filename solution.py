2eed4a4f3e8c21b95b3d85eef3a2febd0dbf3d13
meskt <56514258+meskt@users.noreply.github.com>
2023-04-13 18:33:59 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -5,7 +5,7 @@ from scipy.stats import mannwhitneyu
 
 chat_id = 1841341924 # Ваш chat ID, не меняйте название переменной
 
-def solution(x, y) -> bool: 
+def solution(x,y) -> bool:
     alpha = 0.05
-    pvalue = stats.mannwhitneyu(x, y).pvalue
+    pvalue = stats.ttest_ind(x,y)[1]
     return pvalue < alpha
